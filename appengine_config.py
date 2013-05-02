31fffe94e57feb17412c380306a667f7549d00cf
Hiroaki Nakamura <hnakamur@gmail.com>
2013-05-03 08:07:15 +0900

    Move src/* to the current dir in order to avoid symbolic links. Use skip_files in app.yaml to exclude files during uploading app files.

---
+++ appengine_config.py
@@ -0,0 +1,7 @@
+#-*- encoding: utf-8 -*-
+
+import os
+import sys
+
+sys.path.insert(0,
+    os.path.join(os.path.dirname(__file__), 'lib', 'python2.7', 'site-packages'))
--- src/appengine_config.py
+++
@@ -1,6 +0,0 @@
-#-*- encoding: utf-8 -*-
-
-import os
-import sys
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
